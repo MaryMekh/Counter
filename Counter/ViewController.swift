2a12b805d4708593244226b463f62990717e1572
Mary Mekh <mekhmary@gmail.com>
2024-02-05 05:17:30 +0100

    Добавлен лейбл со значением счетчика и кнопка.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var Label: UILabel!
+    var count = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func Plus(_ sender: Any) {
+         count = count + 1
+        Label.text = "\(count)"
+    }
+    
 
 }
 
